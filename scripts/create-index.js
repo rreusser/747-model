88e0ba1192e2fb86fcc06b9ab3361101a8965595
Ricky Reusser <rsreusser@gmail.com>
2017-05-21 01:24:26 -0400

    🎉

--- scripts/create-index.js
+++ scripts/create-index.js
@@ -9,7 +9,17 @@ var pkg = JSON.parse(fs.readFileSync(require('pkg-up').sync(), 'utf8'));
 
 fromString(html({
   script: 'bundle.min.js',
-  scriptAsync: true
+  scriptAsync: true,
+  body: `<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-50197543-4', 'auto');
+ga('send', 'pageview');
+
+</script>`
 }))
   .pipe(injectMeta(pkg))
   .pipe(injectGithub())
